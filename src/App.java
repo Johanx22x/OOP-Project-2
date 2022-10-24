4b878fc44366755a38e64be6c94e410f1610bd48
Johanx22x <johanrodsa2210@gmail.com>
2022-10-24 09:36:34 -0600

    Create players

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
+    private static Player player1 = null;
+    private static Player player2 = null;
+    private static Player player3 = null;
+    private static Player player4 = null;
+
     public App() {
         new Menu();
+    }
+
+    public static getPlayer1() {
+        return player1;
+    }
+
+    public static getPlayer2() {
+        return player2;
+    }
+
+    public static getPlayer3() {
+        return player3;
+    }
+
+    public static getPlayer4() {
+        return player4;
     }
 }
